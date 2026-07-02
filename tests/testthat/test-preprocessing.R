2c16dd440efa30fa63c36df6c8c6e9f351d553b5
ChristofSeiler <christof.seiler@gmail.com>
2026-07-02 18:35:58 +0200

    add first test case

--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -1,6 +1,11 @@
 test_that("remove missing values", {
-    # sce <- TODO
-    # sce <- removeMissingValues(sce)
-    # expect_equal(sum(is.na(sce)), 0)
-    expect_equal(0, 0)
+    set.seed(23)
+    cell_file <- generate_data()
+    sce <- loadData(cell_file)
+
+    assay(sce)[1, 1] <- NA
+    sce <- removeNAs(sce)
+    n_nas <- sum(is.na(assay(sce)))
+
+    expect_equal(n_nas, 0)
 })
